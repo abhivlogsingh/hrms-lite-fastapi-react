c66453fa3f7c59fd3e05534132b23a9767dee605
Abhishek Singh <abhisheksingh4115@gmail.com>
2026-01-31 18:10:55 +0530

    apis endpoints up

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,14 +4,21 @@ const API = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
-export const getEmployees = () => API.get("/employees");
-export const addEmployee = (data) => API.post("/employees", data);
-export const deleteEmployee = (id) => API.delete(`/employees/${id}`);
+/* ================= EMPLOYEES ================= */
+
+export const getEmployees = () => API.get("/employees/");
+
+export const addEmployee = (data) => API.post("/employees/", data);
+
+export const deleteEmployee = (id) =>
+  API.delete(`/employees/${id}`);
+
+/* ================= ATTENDANCE ================= */
 
 export const getAttendance = (employeeId) =>
   employeeId
     ? API.get(`/attendance/${employeeId}`)
-    : API.get("/attendance");
+    : API.get("/attendance/");
 
-export const addAttendance = (data) => API.post("/attendance", data);
-
+export const addAttendance = (data) =>
+  API.post("/attendance/", data);
